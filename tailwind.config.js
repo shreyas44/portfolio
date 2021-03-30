d874ebc299993cc5df69f9ac15274f5f2067ef18
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-03-30 13:18:13 +0000

    add react icons and gray matter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        content: "fit-content",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
