17ad31efc21fcf1177560c1eadb60da5cd5f03e3
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-03-30 14:56:29 +0000

    update global font and colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        green: colors.green,
+        emarald: colors.emerald,
+      },
       width: {
         content: "fit-content",
       },
