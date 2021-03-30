29972e3be26e336ceffe6f470a862e1601b68521
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-03-30 13:19:29 +0000

    Update navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import Link from "next/link"
 import GitHub from "../icons/github"
 import Medium from "../icons/medium"
 import Twitter from "../icons/twitter"
@@ -11,22 +11,38 @@ const IconContainer = (props) => (
 
 const NavBar: React.FC = (props) => {
   return (
-    <nav className="fixed top-0 right-0 flex items-center mt-6 mr-8 gap-x-3">
-      <a href="https://twitter.com/shreyas4_">
-        <IconContainer>
-          <Twitter />
-        </IconContainer>
-      </a>
-      <a href="https://github.com/shreyas44">
-        <IconContainer>
-          <GitHub />
-        </IconContainer>
-      </a>
-      <a href="https://shreyas-sreenivas.medium.com">
-        <IconContainer>
-          <Medium />
-        </IconContainer>
-      </a>
+    <nav className="fixed top-0 left-0 right-0 flex items-center mx-8 mt-6 bg-white">
+      <div className="absolute top-0 left-0 flex items-center gap-x-3">
+        <a href="#projects" className="hover:underline">
+          Projects
+        </a>
+        <a href="#blog" className="hover:underline">
+          Blog
+        </a>
+        <a href="#contact" className="hover:underline">
+          Contact
+        </a>
+        <Link href="/resume">
+          <a className="hover:underline">Resume</a>
+        </Link>
+      </div>
+      <div className="absolute top-0 right-0 flex gap-x-3">
+        <a href="https://twitter.com/shreyas4_">
+          <IconContainer>
+            <Twitter />
+          </IconContainer>
+        </a>
+        <a href="https://github.com/shreyas44">
+          <IconContainer>
+            <GitHub />
+          </IconContainer>
+        </a>
+        <a href="https://shreyas-sreenivas.medium.com">
+          <IconContainer>
+            <Medium />
+          </IconContainer>
+        </a>
+      </div>
     </nav>
   )
 }
