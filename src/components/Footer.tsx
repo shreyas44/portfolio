8dd5c48648a527e5b514070e591edb8bf23ebfbd
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-03-30 13:19:46 +0000

    Create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer: React.FC = (props) => {
+  return (
+    <div className="absolute inset-x-0 bottom-0 flex items-center justify-center h-10 bg-gray-100">
+      <span className="text-sm font-bold text-gray-500">
+        &copy; Shreyas 2021
+      </span>
+    </div>
+  )
+}
+
+export default Footer
