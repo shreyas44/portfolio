9295dee884391ba8aca3da23beb3fca40ea5e786
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-03-31 09:50:25 +0000

    Use gmail smtp

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -3,7 +3,9 @@ import nodemailer from "nodemailer"
 import { validateName, validateEmail, validateBody } from "../../utils"
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
