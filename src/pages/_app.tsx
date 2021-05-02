5e889dc2cb0a19e601b195a0ee5f42a8751f6477
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-05-02 13:28:07 +0000

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import "../styles/globals.css"
 import "tailwindcss/tailwind.css"
 
+import Head from "next/head"
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1TN9K1TRXW"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {"dataLayer.push(arguments);"}
+          gtag('js', new Date()); gtag('config', 'G-1TN9K1TRXW');
+        </script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
